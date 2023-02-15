f06dd1e780e354c8f39c5f5a02538951126b302f
Rodrigo Vega <rv0611200369@gmail.com>
2023-02-15 14:05:40 -0600

    Modificación Sscción 5,  instalación de paquete para TW

--- src/components/AboutUs/FifthSection.jsx
+++ src/components/AboutUs/FifthSection.jsx
@@ -3,12 +3,12 @@ import SecondLine from './SecondLine';
 
 function FifthSection() {
 	return (
-		<div className="relative py-20 px-8 bg-[rgba(0, 128, 255, 0)] ">
-			<section className="w-full max-w-940 mr-auto ml-auto">
-				<h2 className="text-cobalto font-semibold text-center text-3xl">
+		<div className="bg-[rgba(0, 128, 255, 0)] relative py-20 px-8 ">
+			<section className="mr-auto ml-auto w-full max-w-940">
+				<h2 className="text-center text-3xl font-semibold text-cobalto">
 					Our Story Through the Time
 				</h2>
-				<div className="grid items-start auto-cols-fr gap-y-10 grid-x-10 grid-cols-2 grid-rows-auto1">
+				<div className="grid-x-10 grid auto-cols-fr auto-rows-auto grid-cols-2 items-start gap-y-10">
 					<div className="grid auto-cols-fr grid-rows-auto2 gap-x-10">
 						<FirstLine />
 						<SecondLine />
